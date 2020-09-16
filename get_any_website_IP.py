d09c465777eae96726a74892e6eec80930c53da0
Ismail Ahmed <esmail19980@gmail.com>
2020-09-16 19:19:29 +0200

    Create get_any_website_IP.py

---
+++ get_any_website_IP.py
@@ -0,0 +1,18 @@
+#script written by ismail 
+
+
+#socket is python module resposible for creating a connection between a clinet and server
+import socket
+
+#for activation.. AF_INET stands for IPv4, SOCK_STREAM = TCP protocol 
+s = socket.socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+print(s)
+
+
+#Input the website 
+server = input("Input the name of the website: ")
+
+
+#get the Ip address.
+server_ip = socket.gethostbyname(server)
+print(server_ip)
