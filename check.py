ea85109cec268780c0c07e955ddbc6ba4299b7e5
Ismail Ahmed <esmail19980@gmail.com>
2020-09-29 18:53:20 +0200

    Create check.py

---
+++ check.py
@@ -0,0 +1,13 @@
+import os 
+import socket
+import bs4
+import subprocess
+import sys
+
+server = str(input("Enter the target website: ")
+port = int(input("Input Port number: ")
+
+
+
+
+#will complete it letter...
