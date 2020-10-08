d0d6ca68dc6c0fa88af83c474421d46a4f04dc3e
Ismail Ahmed <esmail19980@gmail.com>
2020-10-08 22:24:38 +0200

    Create ftp_anonymous.py

---
+++ ftp_anonymous.py
@@ -0,0 +1,17 @@
+
+#!/usr/bin/python
+import ftplib
+
+from lib.verify import verify
+
+vuln = ['FTP', '21']
+
+
+def check(url, ip, ports, apps):
+    if verify(vuln, ports, apps):
+        try:
+            ftp = ftplib.FTP(ip)
+            ftp.login('anonymous', 'anonymous')
+            return 'FTP anonymous Login !'
+        except Exception as e:
+            pass
