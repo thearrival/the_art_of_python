9c4e3dc69181240c3044194933b53833c92a322e
Ismail Ahmed <esmail19980@gmail.com>
2020-11-03 19:28:27 +0200

    Update test_your_password.py

--- test_your_password.py
+++ test_your_password.py
@@ -1,11 +1,11 @@
-# A program to check how strong and secure is your password !  by/Ismail_Ahmed
+# A simple program to check how strong and secure is your password !  by/ Ismail_Ahmed
 
-import re 
+import re
 
 pwd = input("Enter your password : ")
 if re.search("[a-z]", pwd) and \
    re.search("[A-Z]", pwd) and \
    re.search("\d",pwd):
    print("Your password is stronger!")
- else: 
-   print("Weak password you should change it !")
+else:
+   print("Weak password! You should change it right nowwww!")
