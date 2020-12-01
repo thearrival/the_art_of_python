afd38c24b33801c03798a29e070494b427224056
Ismail Ahmed <esmail19980@gmail.com>
2020-12-01 10:08:45 +0200

    Create QRcode.py

---
+++ QRcode.py
@@ -0,0 +1,12 @@
+import pyqrcode
+
+import png
+
+from pyqrcode import QRCode
+
+# Paste any url or type anything you want to display it in the QRcode
+QRstring = "https://www.linkedin.com/in/engismail2020/"
+
+url = pyqrcode.create(QRstring)
+
+url.png('Desktop\\qr.png', scale = 9)
