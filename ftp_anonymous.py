1665820ee8310438836a5ce93eff50acdc75f985
Ismail Ahmed <esmail19980@gmail.com>
2023-07-01 14:14:12 +0300

    Update ftp_anonymous.py

--- ftp_anonymous.py
+++ ftp_anonymous.py
@@ -1,12 +1,13 @@
-
 #!/usr/bin/python
+
+
 import ftplib
 
 from lib.verify import verify
 
 vuln = ['FTP', '21']
 
-
+# A function to link port with website URL
 def check(url, ip, ports, apps):
     if verify(vuln, ports, apps):
         try:
