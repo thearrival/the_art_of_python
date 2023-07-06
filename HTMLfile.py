054269740c7baf8eeb6ca32be67aa28564cce525
Ismail Ahmed <esmail19980@gmail.com>
2023-07-06 22:09:05 +0300

    Update HTMLfile.py

--- HTMLfile.py
+++ HTMLfile.py
@@ -1,5 +1,9 @@
+#URLOPEN is a python module that works to gather html file for any website 
+
 from urllib.request import urlopen
 
+
+# Now we are going to load the target website
 html = urlopen(input(" Enter the website URL: "))
 print("-"*60)
 print(html.read())
